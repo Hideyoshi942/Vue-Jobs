f3b126bc5dc7f8692b16739ef101aef3ac16c042
Chu Tuấn Kiệt <96521130+Hideyoshi942@users.noreply.github.com>
2024-12-10 16:07:28 +0700

    Vue Jobs JSON

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCars from "@/components/HomeCars.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+  <Hero/>
+  <HomeCars />
+  <JobListings :limit="3" :showButton="true"/>
+</template>
+
+<style scoped>
+
+</style>
